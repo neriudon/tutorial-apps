f915b0fc322d62cbf2b28bbb53302729743b2777
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2017-08-07 15:00:23 +0900

    Modified code format(java) at src/test directory #60

--- first-spring-security/src/test/java/com/example/security/selenium/FirstSpringSecurityTest.java
+++ first-spring-security/src/test/java/com/example/security/selenium/FirstSpringSecurityTest.java
@@ -38,10 +38,10 @@ public class FirstSpringSecurityTest extends FunctionTestSupport {
 
         // check initial value
         {
-        	assertThat(inputFieldAccessor.getValue(By.id("j_username"),driver),
-            		is("demo"));
-            assertThat(inputFieldAccessor.getValue(By.id("j_password"),driver),
-            		is("demo"));
+            assertThat(inputFieldAccessor.getValue(By.id("j_username"), driver),
+                    is("demo"));
+            assertThat(inputFieldAccessor.getValue(By.id("j_password"), driver),
+                    is("demo"));
 
         }
 
@@ -62,13 +62,13 @@ public class FirstSpringSecurityTest extends FunctionTestSupport {
 
         // check home screen
         {
-            assertThat(driver.findElement(By.tagName("h1")).getText(),
-                    is("Hello world!"));
+            assertThat(driver.findElement(By.tagName("h1")).getText(), is(
+                    "Hello world!"));
 
-            assertThat(driver.findElement(By.tagName("p")).getText(), is("Welcome Taro Yamada"));
-            assertThat(
-            		driver.findElement(By.xpath("//a[@href='/"
-                            + contextName + "/account']")).getText(), is("view account"));
+            assertThat(driver.findElement(By.tagName("p")).getText(), is(
+                    "Welcome Taro Yamada"));
+            assertThat(driver.findElement(By.xpath("//a[@href='/" + contextName
+                    + "/account']")).getText(), is("view account"));
         }
 
     }
@@ -86,16 +86,16 @@ public class FirstSpringSecurityTest extends FunctionTestSupport {
 
         // click logout button
         {
-        	driver.findElement(By.xpath("//a[@href='/" + contextName
+            driver.findElement(By.xpath("//a[@href='/" + contextName
                     + "/logout']")).click();
         }
 
         // check login screen
         {
-        	assertThat(inputFieldAccessor.getValue(By.id("j_username"),driver),
-            		is("demo"));
-            assertThat(inputFieldAccessor.getValue(By.id("j_password"),driver),
-            		is("demo"));
+            assertThat(inputFieldAccessor.getValue(By.id("j_username"), driver),
+                    is("demo"));
+            assertThat(inputFieldAccessor.getValue(By.id("j_password"), driver),
+                    is("demo"));
         }
 
     }
@@ -115,15 +115,16 @@ public class FirstSpringSecurityTest extends FunctionTestSupport {
 
         // click "view account" button.
         {
-        	driver.findElement(By.xpath("//a[@href='/" + contextName
-                    + "/account']")).click();;
+            driver.findElement(By.xpath("//a[@href='/" + contextName
+                    + "/account']")).click();
+            ;
         }
 
         // check account information
         {
             // get table
-            List<WebElement> cheese = driver.findElements(By
-                    .xpath("//table/tbody/tr/td"));
+            List<WebElement> cheese = driver.findElements(By.xpath(
+                    "//table/tbody/tr/td"));
 
             assertThat(cheese.get(0).getText(), is("demo"));
             assertThat(cheese.get(1).getText(), is("Taro"));
@@ -142,20 +143,21 @@ public class FirstSpringSecurityTest extends FunctionTestSupport {
 
         // login as an unregistered user
         {
-            inputFieldAccessor.overrideValue(By.id("j_username"), "aaa", driver);
+            inputFieldAccessor.overrideValue(By.id("j_username"), "aaa",
+                    driver);
             driver.findElement(By.name("submit")).click();
 
         }
 
         // check error message and check login screen
         {
-            assertThat(driver.findElement(By.tagName("li")).getText(),
-                    is("Bad credentials"));
+            assertThat(driver.findElement(By.tagName("li")).getText(), is(
+                    "Bad credentials"));
 
-            assertThat(inputFieldAccessor.getValue(By.id("j_username"),driver),
-            		is("demo"));
-            assertThat(inputFieldAccessor.getValue(By.id("j_password"),driver),
-            		is("demo"));
+            assertThat(inputFieldAccessor.getValue(By.id("j_username"), driver),
+                    is("demo"));
+            assertThat(inputFieldAccessor.getValue(By.id("j_password"), driver),
+                    is("demo"));
         }
     }
 }
