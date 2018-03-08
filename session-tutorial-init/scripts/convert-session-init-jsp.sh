b32e2757a62f239cbdbaa91b5c8d85c871125eee
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2018-03-08 17:07:11 +0900

    improvement shell #181
    (cherry picked from commit 425e894e211c96d3a5877b4103284eda191c1303)

--- session-tutorial-init/scripts/convert-session-init-jsp.sh
+++ session-tutorial-init/scripts/convert-session-init-jsp.sh
@@ -3,14 +3,14 @@
 # Parameters:
 #   $1 : (Optional) Target project path to convert.
 
-APPLICATION_DIR=$1
-if test -n $APPLICATION_DIR; then
-  pushd "$APPLICATION_DIR"
+TARGET_DIR=$1
+if test -n $TARGET_DIR; then
+  pushd "${TARGET_DIR}/${ARTIFACT_ID}"
 fi
 
 # include.jsp
 find ./ -type f -name 'include.jsp' | xargs sed -i -e 's|<%@ page session="false"|<%@ page session="true"|'
 
-if test -n $APPLICATION_DIR; then
+if test -n $TARGET_DIR; then
   popd
 fi
