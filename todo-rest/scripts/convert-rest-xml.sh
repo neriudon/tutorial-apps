fb0d572f0f3966127ceda9530ebe82d54170b003
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2018-03-09 11:20:18 +0900

    improvement directory check #181
    (cherry picked from commit 48b7a9b0116ce54bcd53761204827242697c50e0)

--- todo-rest/scripts/convert-rest-xml.sh
+++ todo-rest/scripts/convert-rest-xml.sh
@@ -4,7 +4,7 @@
 #   $1 : (Optional) Target project path to convert.
 
 TARGET_DIR=$1
-if test -n $TARGET_DIR; then
+if test -n "${TARGET_DIR}/${ARTIFACT_ID}"; then
   pushd "${TARGET_DIR}/${ARTIFACT_ID}"
 fi
 
@@ -44,6 +44,6 @@ find ./ -type f -name 'spring-security.xml' | xargs sed -i -e 's|<sec:http patte
     </sec:http>\
 |'
 
-if test -n $TARGET_DIR; then
+if test -n "${TARGET_DIR}/${ARTIFACT_ID}"; then
   popd
 fi
